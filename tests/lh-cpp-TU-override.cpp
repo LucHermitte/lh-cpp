40c35009d9b5f334a81b97c76afd925afba2a223
luc.hermitte <luc.hermitte@gmail.com>
2008-04-23 01:07:04 +0000

    - lh-cpp : Work on :Override is continuing.

---
+++ tests/lh-cpp-TU-override.cpp
@@ -0,0 +1,63 @@
+// Vim: let b:tags_dirname = '.'
+//
+namespace foo { 
+    class A0
+    {
+	virtual ~A0() = 0;
+	virtual void a0();
+    };
+
+    class A1
+    {
+	virtual ~A1();
+	virtual void f1(std::string const& s) = 0;
+	virtual void f2() { f1("foo"); }
+	void f2(int);
+
+[]
+    };
+
+    class A2 : virtual A0 
+    {
+	virtual ~A2() {}
+	virtual void g();
+	virtual void a0();
+
+['A0']
+    }
+
+    class A3 : virtual A0
+    {
+	void a0();
+
+['A0']
+    }
+
+    class C1 : A1, A2
+    {
+	virtual ~C1();
+	void f2(); 
+
+['A0', 'A1', 'A2']
+    };
+
+    class D : C1, virtual A0
+    {
+	virtual void g();
+
+['A0', 'A1', 'A2', 'C1']
+    };
+} // foo namespace 
+
+namespace bar { 
+    struct V {};
+    struct Z {};
+    struct C1 : virtual V {};
+    struct C2 : virtual V, Z {};
+    struct C3 : C2{};
+    struct D : C1, C3 {};
+// echo lh#cpp#AnalysisLib_Class#Ancestors('D')
+} // bar namespace 
+
+
+
