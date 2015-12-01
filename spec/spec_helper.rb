32519043b5767f52287baf8c73484b83fd6943a4
Luc Hermitte <luc.hermitte@gmail.com>
2015-12-01 10:46:32 +0100

    BUG: redundant spec code removed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -70,16 +70,6 @@ RSpec.configure do |config|
 
     File.open(filename, 'w') { |f| f.write(contents) }
   end
-
-  # Execute each example in its own temporary directory that is automatically
-  # destroyed after every run.
-  config.around do |example|
-    Dir.mktmpdir do |dir|
-      Dir.chdir(dir) do
-        example.call
-      end
-    end
-  end
 end
 
 # vim:set sw=2:
