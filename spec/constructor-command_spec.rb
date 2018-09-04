bff36e8ce75a8b658a9d5b0c0a1ce27ca516a12e
Luc Hermitte <luc.hermitte@gmail.com>
2018-09-04 16:36:19 +0000

    TST: Check proto generated

--- spec/constructor-command_spec.rb
+++ spec/constructor-command_spec.rb
@@ -136,8 +136,8 @@ RSpec.describe ":Constructor command", :cpp, :ctr_cmd do
 
     it "has a pointer attribute" do # {{{3
       vim.command('let g:mocked_confirm = 0')
-      expect(vim.echo('lh#dev#class#attributes("Foo")')).to eq('m_bar')
-      expect(vim.echo('lh#cpp#constructors#debug("s:Attributes(\"Foo\")")')).to eq('m_bar')
+      # expect(vim.echo('lh#dev#class#attributes("Foo")')).to eq('m_bar')
+      # expect(vim.echo('lh#cpp#constructors#debug("s:Attributes(\"Foo\")")')).to eq('m_bar')
       vim.command('Constructor assign')
       expect(vim.echo('g:step."--".string(g:implproto)')).to eq('42')
       expect(vim.echo('g:step')).to eq('42')
