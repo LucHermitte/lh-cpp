d5934cb61a6bcde324b43c6bda17c18bd253e146
Luc Hermitte <luc.hermitte@gmail.com>
2024-08-29 03:14:52 +0200

    TST: Investigate missing snippets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,15 @@ module Vimrunner
         script_path = Path.new(script)
         command("runtime #{script_path}")
     end
+
+    def prepend_rtp(dir)
+      dir_path = Path.new(dir)
+      if File.directory?(dir.to_s)
+        prepend_runtimepath(dir)
+      else
+        pp "Path #{dir_path} doesn't exist"
+      end
+    end
   end
 
   module Platform
@@ -49,39 +58,39 @@ Vimrunner::RSpec.configure do |config|
   config.start_vim do
     vim = Vimrunner::Server.new(:executable => Vimrunner::Platform.best_vim, :vimrc => vimrc).start
     vim.add_plugin(vim_flavor_path, 'bootstrap.vim')
-    vim.prepend_runtimepath(vim_plugin_path+'/after')
-    vim.prepend_runtimepath(vim_plugin_path)
+    vim.prepend_rtp(vim_plugin_path+'/after')
+    vim.prepend_rtp(vim_plugin_path)
 
     # lh-UT
     vim_UT_path      = File.expand_path('../../../vim-UT', __FILE__)
-    vim.prepend_runtimepath(vim_UT_path)
+    vim.prepend_rtp(vim_UT_path)
     vim.runtime('plugin/UT.vim')
 
     # pp vim_flavor_path
     # lh-vim-lib
     vim_lib_path      = File.expand_path('../../../lh-vim-lib', __FILE__)
-    vim.prepend_runtimepath(vim_lib_path)
+    vim.prepend_rtp(vim_lib_path)
     vim.runtime('plugin/let.vim') # LetIfUndef
     # vim.runtime('plugin/ui-functions.vim') # lh#ui#confirm
     # vim.command(':messages')
 
     # lh-tags
     vim_tags_path = File.expand_path('../../../lh-tags', __FILE__)
-    vim.prepend_runtimepath(vim_tags_path)
+    vim.prepend_rtp(vim_tags_path)
     vim.runtime('plugin/lh-tags.vim') # AddStyle
 
     # lh-style
     vim_style_path = File.expand_path('../../../lh-style', __FILE__)
-    vim.prepend_runtimepath(vim_style_path)
+    vim.prepend_rtp(vim_style_path)
     vim.runtime('plugin/lh-style.vim') # AddStyle
 
     # lh-dev
     vim_dev_path = File.expand_path('../../../lh-dev', __FILE__)
-    vim.prepend_runtimepath(vim_dev_path)
+    vim.prepend_rtp(vim_dev_path)
 
     # lh-brackets
     vim_brackets_path = File.expand_path('../../../lh-brackets', __FILE__)
-    vim.prepend_runtimepath(vim_brackets_path)
+    vim.prepend_rtp(vim_brackets_path)
     vim.runtime('plugin/misc_map.vim') # Inoreab
     vim.runtime('plugin/common_brackets.vim') # Brackets
     vim.runtime('plugin/bracketing.base.vim') # !mark!, !jump!
@@ -90,8 +99,8 @@ Vimrunner::RSpec.configure do |config|
 
     # mu-template
     mu_template_path = File.expand_path('../../../mu-template@lh', __FILE__)
-    vim.prepend_runtimepath(mu_template_path+'/after')
-    vim.prepend_runtimepath(mu_template_path)
+    vim.prepend_rtp(mu_template_path+'/after')
+    vim.prepend_rtp(mu_template_path)
     vim.runtime('plugin/mu-template.vim')
 
     pp vim.echo('"RTP -> " . &rtp')
